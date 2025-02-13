8f7dd1356c70be20124ad6d4fb8fa24806f2345f
Zeyad Hassan <zeyad.hassan.balal@gmail.com>
2025-02-13 10:33:43 +0200

    All main services implemented🪛
    Users service
    Posts service
    Comments service

--- src/app/core/services/comments.service.ts
+++ src/app/core/services/comments.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/env';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
 
-  constructor() { }
+  constructor(private _HttpClient: HttpClient) { }
+
+  createComment(data: object): Observable<any> {
+
+    return this._HttpClient.post(`${environment.baseUrl}/comments`, data)
+  }
+  getPostComments(postId: string): Observable<any> {
+      
+      return this._HttpClient.get(`${environment.baseUrl}/posts/${postId}/comments`)
+  }
+
+  upadteCommment(commentId: string, data: object): Observable<any> {
+    return this._HttpClient.put(`${environment.baseUrl}/comments/${commentId}`, data)
+  }
+
+  deleteCommment(commentId: string): Observable<any> {
+    return this._HttpClient.delete(`${environment.baseUrl}/comments/${commentId}`)
+  }
+
 }
--- src/app/core/services/posts.service.ts
+++ src/app/core/services/posts.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/env';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private _HttpClient: HttpClient) { }
+
+  creatPost(data: object):Observable<any> {
+    return this._HttpClient.post(`${environment.baseUrl}/posts`, data)
+  }
+
+  getAllPosts():Observable<any> {
+    return this._HttpClient.get(`${environment.baseUrl}/posts`)
+  }
+
+  getMyPosts():Observable<any> {
+    return this._HttpClient.get(`${environment.baseUrl}/users/664bcf3e33da217c4af21f00/posts`)
+  }
+   
+  getSinglePost(postId:string):Observable<any> {
+    return this._HttpClient.get(`${environment.baseUrl}posts/${postId}`)
+  }
+
+ updatePost(data:object,postId:string):Observable<any> {
+    return this._HttpClient.put(`${environment.baseUrl}posts/${postId}`,data)
+  }
+
+  deletePost(postId:string):Observable<any> {
+    return this._HttpClient.delete(`${environment.baseUrl}posts/${postId}`)
+  }
+
 }
